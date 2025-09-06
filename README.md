e7a0785e8378f2dab9f9b4bde32ecbf52daa8d8e
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-09-06 12:29:05 -0300

    creating eks cluster and network

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # eks-terraform-stack
+
+-> criação de usuário no IAM - AWS
+-> criação de role no IAM - AWS
+-> tfsate lock no s3 terraform
---
+++ terraform/backend/variables.tf
@@ -0,0 +1,35 @@
+variable "tags" {
+  type = map(string)
+  default = {
+    Project     = "eks-terraform-stack"
+    Environment = "prod"
+  }
+}
+
+variable "auth" {
+  type = object({
+    assume_role_arn = string
+    region          = string
+  })
+
+  default = {
+    assume_role_arn = "arn:aws:iam::567349023925:role/workshop-role"
+    region          = "us-east-1"
+  }
+}
+
+variable "remote_backend" {
+  type = object({
+    s3_bucket                   = string
+    dynamodb_table_name         = string
+    dynamodb_table_billing_mode = string
+    dynamodb_table_hash_key     = string
+  })
+
+  default = {
+    s3_bucket                   = "s3-remote-backend-bucket"
+    dynamodb_table_name         = "s3-state-locking-table"
+    dynamodb_table_billing_mode = "PAY_PER_REQUEST"
+    dynamodb_table_hash_key     = "LockID"
+  }
+}
